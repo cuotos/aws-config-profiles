c65404302b1b4e8832c54ec7a5cd08f37f3e2460
Dan Potepa <dan@danpotepa.co.uk>
2022-10-10 17:56:10 +0100

    added long output to print key if in config file

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
 	"text/tabwriter"
 
 	"gopkg.in/ini.v1"
+)
+
+var (
+	appVersion string = "not provided"
+	fullOutput        = false
 )
 
 func main() {
@@ -17,6 +23,19 @@ func main() {
 }
 
 func run() error {
+
+	if len(os.Args) == 2 {
+		switch arg := os.Args[1]; arg {
+		case "-v":
+			fmt.Println(appVersion)
+			os.Exit(0)
+		case "-l":
+			fullOutput = true
+		default:
+			fmt.Printf("unknown flag %s\n", arg)
+			os.Exit(1)
+		}
+	}
 
 	userHomeDir, err := os.UserHomeDir()
 	if err != nil {
@@ -43,26 +62,38 @@ func run() error {
 			p.AccountNumber = section.Key("sso_account_id").String()
 		}
 
+		if section.HasKey("aws_access_key_id") {
+			p.AccessKeyId = section.Key("aws_access_key_id").String()
+		}
+
 		profiles = append(profiles, p)
 	}
 
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
 
 	for _, p := range profiles {
-		last4 := ""
-		if len(p.AccountNumber) > 4 {
-			last4 = p.AccountNumber[8:]
-		}
-		fmt.Fprintf(w, "%s\t%s\t%s\t\n", last4, p.AccountNumber, p.Name)
+		write(w, p, fullOutput)
 	}
 
 	w.Flush()
 
 	return nil
+}
 
+func write(w io.Writer, profile Profile, long bool) {
+	last4 := ""
+	if len(profile.AccountNumber) > 4 {
+		last4 = profile.AccountNumber[8:]
+	}
+	if long {
+		fmt.Fprintf(w, "%s\t%s\t%s\t%s\t\n", last4, profile.AccountNumber, profile.AccessKeyId, profile.Name)
+	} else {
+		fmt.Fprintf(w, "%s\t%s\t%s\t\n", last4, profile.AccountNumber, profile.Name)
+	}
 }
 
 type Profile struct {
 	Name          string
 	AccountNumber string
+	AccessKeyId   string
 }
